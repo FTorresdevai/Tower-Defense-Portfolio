e65ab611637a9e5071799b9b4011823d138f93c1
FTowers06 <up202406332@up.pt>
2025-11-09 22:19:30 +0000

    Element inicializado

---
+++ src/main/java/Element.java
@@ -0,0 +1,11 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public abstract class Element {
+    private Position pos;
+
+    public Element(Position position) { this.pos = position; }
+    public Position getPosition() { return pos; }
+    public void setPosition(Position position) { this.pos= position; }
+
+    public abstract void draw(TextGraphics graphics);
+}
