476007a02419ce7cee5da398a23722b0823b748b
FTowers06 <up202406332@up.pt>
2025-11-29 17:35:07 +0000

    Falta corrigir bug da hitbox mas esta melhor

--- src/main/java/game/Path.java
+++ src/main/java/game/Path.java
@@ -53,27 +53,33 @@ public class Path {
         }
     }
 
-    public boolean isOnPath(Position position) {
-        int x = (int) position.getX();
-        int y = (int) position.getY();
+    public boolean isOnPath(Position position, Shape shape) {
+        int minDx = 0; int maxDx = 0;
+        int minDy = 0; int maxDy = 0;
+
+        for (Pixel p : shape.getPixels()) {
+            if (p.getDx() < minDx) minDx = p.getDx();
+            if (p.getDx() > maxDx) maxDx = p.getDx();
+            if (p.getDy() < minDy) minDy = p.getDy();
+            if (p.getDy() > maxDy) maxDy = p.getDy();
+        }
+
+        int tMinX = (int) position.getX() + minDx;
+        int tMaxX = (int) position.getX() + maxDx;
+        int tMinY = (int) position.getY() + minDy;
+        int tMaxY = (int) position.getY() + maxDy;
 
         for (int i = 0; i < nodes.size() - 1; i++) {
             Position p1 = nodes.get(i);
             Position p2 = nodes.get(i + 1);
 
-            int x1 = (int) p1.getX();
-            int y1 = (int) p1.getY();
-            int x2 = (int) p2.getX();
-            int y2 = (int) p2.getY();
+            int pMinX = Math.min((int) p1.getX(), (int) p2.getX());
+            int pMaxX = Math.max((int) p1.getX(), (int) p2.getX());
+            int pMinY = Math.min((int) p1.getY(), (int) p2.getY());
+            int pMaxY = Math.max((int) p1.getY(), (int) p2.getY());
 
-            if (x1 == x2 && x == x1) {
-                if (y >= Math.min(y1, y2) && y <= Math.max(y1, y2)) {
-                    return true;
-                }
-            } else if (y1 == y2 && y == y1) {
-                if (x >= Math.min(x1, x2) && x <= Math.max(x1, x2)) {
-                    return true;
-                }
+            if (tMaxX >= pMinX && tMinX <= pMaxX && tMaxY >= pMinY && tMinY <= pMaxY) {
+                return true;
             }
         }
         return false;
